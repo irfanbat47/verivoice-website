7f554e1f744971cb4de27c6f3b75f96a1094122b
Mohammed Irfan <92452354+irfanbat47@users.noreply.github.com>
2024-08-26 21:13:21 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+// JavaScript for smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
-
         document.querySelector(this.getAttribute('href')).scrollIntoView({
             behavior: 'smooth'
         });
+    });
+});
+
+// Learn More button scroll to Features section
+document.getElementById('learn-more').addEventListener('click', function() {
+    document.getElementById('features').scrollIntoView({
+        behavior: 'smooth'
     });
 });
